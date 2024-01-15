186133f7db1d4586e6ccd4beb0d507f92a204cfd
Johanna Hakonen <48389769+hannahakonen@users.noreply.github.com>
2024-01-15 20:42:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './components/Footer'
+
+
+const App = () => {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Physio!</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App 
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  const footerStyle = {
+    color: 'lightgrey',
+    background: 'rgb(114, 98, 130)',
+    fontSize: 16
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <span>&copy; 2023 Johanna Hakonen</span>
+    </div>
+  )
+}
+
+export default Footer
